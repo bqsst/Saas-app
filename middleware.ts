90d2375fab2ea37f556e7cd19d8353b0318c56d1
Abbas Leenud <152507598+bqsst@users.noreply.github.com>
2024-12-13 22:39:59 +0700

    update middleware

--- middleware.ts
+++ middleware.ts
@@ -1,13 +1,14 @@
-import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
+import { clerkMiddleware } from "@clerk/nextjs/server";
 
-const publicRoutes = createRouteMatcher(['/sign-in', '/sign-up', '/'])
+const publicRoutes = ['/sign-in', '/sign-up', '/'];
 
 export default clerkMiddleware((auth, req) => {
-  if (!publicRoutes(req)) {
-    return auth().protect();
+  if (publicRoutes.some(route => req.nextUrl.pathname.includes(route))) {
+    return new Response(null);
   }
-})
+  return new Response("Auth required", { status: 401 });
+});
 
 export const config = {
-  matcher: ['/((?!.*\\..*_next).*)/', '/', '/api[|trpc](.*)'],
+  matcher: ['/((?!.*\\..*|_next).*)/', '/', '/api(.*)'],
 };
